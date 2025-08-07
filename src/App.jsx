d8c2466fd73866d9dc0454d526bdcba832737235
Stanislav <s.v.pukha@gmail.com>
2025-08-07 16:27:29 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button, Input, Card, CardBody } from "@heroui/react";
+
+function App() {
+
+  return (
+    <main>
+      <Card>
+        <CardBody>
+          <h1 className="text-3xl text-red-600">agreements-hub-tauri</h1>
+          <Button>Button</Button>
+          <Input />
+        </CardBody>
+      </Card>
+    </main>
+  );
+}
+
+export default App;
